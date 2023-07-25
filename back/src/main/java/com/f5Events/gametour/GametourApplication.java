2db86b3523abe24ea4d37ea91c67b84f6b440209
PabloEsteban87 <pablosebas87@gmail.com>
2023-07-25 14:47:01 +0200

    back and front

---
+++ back/src/main/java/com/f5Events/gametour/GametourApplication.java
@@ -0,0 +1,13 @@
+package com.f5Events.gametour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametourApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/f5Events/gametour/GametourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5Events.gametour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
