f8b66a5b48fe80da41398cb6522cfe43396b6676
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-28 14:35:57 +0200

    appmodules change route

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LoginregisterComponent } from './events/components/loginregister/loginregister.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    LoginregisterComponent
   ],
   imports: [
     BrowserModule,
