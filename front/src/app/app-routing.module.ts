9ee29d21cc6113bb0b68fc3a1f4dceefa1025e11
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-07-31 15:54:11 +0200

    edit gitignore

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CombinedPageComponent } from './Events/components/combined-page/combined-page.component';
+import { LoginregisterComponent } from './Events/components/loginregister/loginregister.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
+ 
+    { path: '', component: LoginregisterComponent, },
+    { path: 'Footer', component: FooterComponent },
+    
+  ];
 
-  { path: 'combined-page', component: CombinedPageComponent },
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
