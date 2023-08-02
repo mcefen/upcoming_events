3b57a6f9ef394add0699182ead83262924f97cca
PabloEsteban87 <pablosebas87@gmail.com>
2023-08-02 15:41:44 +0200

    merge develop

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Events/views/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginregisterComponent } from './Events/components/loginregister/loginregister.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PageregisterComponent } from './Events/components/pageregister/pageregister.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginregisterComponent,
+    FooterComponent,
+    PageregisterComponent,
+
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
