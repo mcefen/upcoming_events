9922aeffa7ddf3fed2825a66e128b8cebc505bbb
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-08-04 13:21:28 +0200

    Modificate BBDD

--- back/src/main/java/com/f5Events/gametour/models/EventGames.java
+++ back/src/main/java/com/f5Events/gametour/models/EventGames.java
@@ -1,131 +1,85 @@
 package com.f5Events.gametour.models;
 
 import java.sql.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "eventgames")
 public class EventGames {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_eventgame")
     private Long id;
-    
     @Column(name = "title")
     private String title;
-
     @Column(name = "publicationevent")
-
     private Date publicationevent;
-
     @Column(name = "participants")
-    private Number participants;
-
+    private Integer participants;
     @Column(name = "limitparticipants")
-    private Number limitparticipants;
- 
+    private Integer limitparticipants;
     @Column(name = "description")
     private String description;
-
     @Column(name = "image")
     private String image;
-    
-    @Column(name = "availability")
-    private String availability;
-
-    @ManyToOne
-    @JoinColumn(name = "id_game")
-
-    private Games idgame;
-
-    @ManyToOne
-    @JoinColumn(name = "id_user")
-
+     @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "id_game", unique=false, nullable=true, insertable=true, updatable=true)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+     private Games idgame;
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "id_user", unique=false, nullable=true, insertable=true, updatable=true)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private User iduser;
-    
-    
-    
-    
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public Date getPublicationevent() {
         return publicationevent;
     }
-
     public void setPublicationevent(Date publicationevent) {
         this.publicationevent = publicationevent;
     }
-
-    public Number getParticipants() {
+    public Integer getParticipants() {
         return participants;
     }
-
-    public void setParticipants(Number participants) {
+    public void setParticipants(Integer participants) {
         this.participants = participants;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getImage() {
         return image;
     }
-
     public void setImage(String image) {
         this.image = image;
     }
-
-      public Number getLimitparticipants() {
+      public Integer getLimitparticipants() {
         return limitparticipants;
     }
-
-    public void setLimitparticipants(Number limitparticipants) {
+    public void setLimitparticipants(Integer limitparticipants) {
         this.limitparticipants = limitparticipants;
     }
-
-       public String getAvailability() {
-        return availability;
-    }
-
-    public void setAvailability(String availability) {
-        this.availability = availability;
-    }
-
-        public Games getIdgame() {
+     public Games getIdgame() {
         return idgame;
     }
-
     public void setIdgame(Games idgame) {
         this.idgame = idgame;
     }
-
       public User getIduser() {
         return iduser;
     }
-
     public void setIduser(User iduser) {
         this.iduser = iduser;
     }
-
-    
 }
