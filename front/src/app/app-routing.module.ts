9e2ec2f6ab2cc17435c9ed536e33214039f7fc4d
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-08-28 08:53:43 +0200

    routes to aplication

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginregisterComponent } from './Events/components/loginregister/loginregister.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AppComponent } from './app.component';
+import { PageregisterComponent } from './Events/components/pageregister/pageregister.component';
+import { HomeComponent } from './Events/views/home/home.component';
 
 const routes: Routes = [
   
-    { path: '', component: AppComponent},
-    { path: 'Login', component: LoginregisterComponent, },
-    { path: 'Footer', component: FooterComponent },
-    
-  ];
+  { path: '', component: AppComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginregisterComponent, },
+  { path: 'footer', component: FooterComponent },
+  { path: 'register', component: PageregisterComponent },
+];
+
 
 
 @NgModule({
